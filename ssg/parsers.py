e87475f3fd938f2f9313bbf67e05f9a361720c19
rehanmiah <rehan_miah@hotmail.com>
2021-01-16 18:00:55 +0000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,4 +3,4 @@ from typing import List
 class Parser:
     extensions: List[str] = []
     def valid_extension(self,extension):
-        return self.extension
+        return extension in self.extensions
