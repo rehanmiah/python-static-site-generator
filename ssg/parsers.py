0382f0e4ec4c93d95bd58eb3f1779c9a3fb05a33
rehanmiah <rehan_miah@hotmail.com>
2021-01-16 18:14:59 +0000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,8 +4,9 @@ class Parser:
     extensions: List[str] = []
     def valid_extension(self,extension):
         return extension in self.extensions
-    def parse(self, path, source, dest):
-        return "hell"
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
 
     def write(self,path,dest,content,ext=".html"):
         full_path=dest / with_suffix(ext)
