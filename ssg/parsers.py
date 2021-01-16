1a5203e5b2adb7414dbe5d2ddd55b1c10ec826c6
rehanmiah <rehan_miah@hotmail.com>
2021-01-16 18:27:40 +0000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,7 +9,7 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path):
-        with open(path,r) as file:
+        with open(path,"r") as file:
           return file.read()
     def write(self,path,dest,content,ext=".html"):
         full_path=dest / with_suffix(ext)
