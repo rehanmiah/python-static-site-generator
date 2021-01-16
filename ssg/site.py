edc3fa41f0b00dc1dc0e5ff2231442c1482029dc
rehanmiah <rehan_miah@hotmail.com>
2021-01-16 16:22:49 +0000

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -3,9 +3,10 @@ class Site:
     def __init__(self,source,dest):
         self.source = Path(source)
         self.dest = Path(dest)
-    def create_dir(self,Path):
-        destination=self.dest / Path.relative_to(self.source)
+    def create_dir(self,path):
+        directory = self.dest / path.relative_to(self.source)
         mkdir(self.dest)
+
 
     def build(self):
         mkdir(self.dest)
