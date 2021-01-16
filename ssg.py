8738ac86eee4c78f22e0fedf6512a549256a912e
rehanmiah <rehan_miah@hotmail.com>
2021-01-16 17:33:59 +0000

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -1,7 +1,7 @@
 import typer
-from ssg.site import site
+from ssg.site import Site
 
 def main(source="content",dest="dist:):
     config={"source"=source,"dest"=dest}
 
-typer.run(__main__)
+typer.run(main)
