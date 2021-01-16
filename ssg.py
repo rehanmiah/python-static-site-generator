2baf8a249b930e2f853b126e623ab88eb5c68db7
Rehan Miah <rmi46@uk4585436.home>
2021-01-16 15:46:41 +0000

    rehan

---
+++ ssg.py
@@ -0,0 +1,7 @@
+import typer
+import site from ssg.site
+
+def __main__(source="content",dest="dist:):
+    config={}
+
+typer.run(__main__)
---
+++ ssg/site.py
@@ -0,0 +1,18 @@
+import path from pathlib
+class Site:
+    def __init__(self,source,dest):
+        self.source=path(source)
+        self.dest=path(dest)
+    def create_dir(self,path):
+        self.dest=destination / relative_to()
+        mkdir(self.dest)
+
+    def build(self):
+        mkdir(self.dest)
+        for j in self.source.rglob("*"):
+            if j==dircetory:
+                create_dir(j)
+
+
+
+
