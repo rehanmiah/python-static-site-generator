16b0425c306f5c15aca3f8452d4900b9a2247b12
rehanmiah <rehan_miah@hotmail.com>
2021-01-16 21:22:40 +0000

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -1,3 +1,51 @@
 from collections.abc import Mapping
 import re
 from yaml import FullLoader, load
+
+class Content(Mapping):
+    __delimeter="^(?:-|\+){3}\s*$"
+    __regex=re.vompile(__delimeter,re.MULTILINE)
+
+    def load(self, cls,string):
+        __, fm, content=__regex.split(string,2)
+
+    load(fm,Loader=FullLoader)
+    cls(metadata, content)
+
+class Content():
+    def __init__(self, metadata, content):
+        self.data=metadata
+        self.data={"content":content}
+
+    @property:
+    body():
+    return self.data["content"]
+
+    @propert:
+    type():
+    if self.data["type"] is not set:
+        return None
+    else:
+        return self.data["type"]
+
+    @property:
+    type()
+    self.data["type"]
+
+    def __getitem__(self,key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data()
+
+    def __len__(self)
+        return self.data.length()
+
+    def __rrepr__(self):
+        data={}
+        return str(data)
+    for value in self.data.items():
+        if key!="content":
+            data[key]=value
+
+
