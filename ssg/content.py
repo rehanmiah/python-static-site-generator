75b150a07f965b2ecfcd0f59dab6ca7c93f15034
rehanmiah <rehan_miah@hotmail.com>
2021-01-16 21:30:56 +0000

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -3,8 +3,8 @@ import re
 from yaml import FullLoader, load
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex=re.vompile(__delimeter,re.MULTILINE)
+    __delimeter = r"^(?:-|\+){3}\s*$"
+    __regex=re.compile(__delimeter,re.MULTILINE)
 
     def load(self, cls,string):
         __, fm, content=__regex.split(string,2)
