e5d0b868cf8a7f813c5feca8053e511e236443e8
rehanmiah <rehan_miah@hotmail.com>
2021-01-16 21:46:26 +0000

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -6,12 +6,12 @@ class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
+
     @classmethod
     def load(cls, string):
-        _, fm, content = cls.__regex.split(string, 2)
-        metadata = load(fm, Loader=FullLoader)
+        __, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm,Loader=FullLoader)
         return cls(metadata, content)
-
 
 class Content():
     def __init__(self, metadata, content):
