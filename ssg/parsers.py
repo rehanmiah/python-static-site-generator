df829464540c8756cdaf6d92991f8d12480e5099
rehanmiah <rehan_miah@hotmail.com>
2021-01-17 19:31:35 +0000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -16,6 +16,9 @@ class MarkdownParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content)
         )
+        html=publish_parts(content.body, writer_name, "html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write()
 
 
 
@@ -24,6 +27,9 @@ class ReStructuredTextParser(Parser):
     extensions = [".rst"]
     def parse(self, path: Path, source: Path, dest: Path):
         content=Content.load(self.read(path))
+    html=publish_Parts(content.body, writer_name="html")
+    self.write(path, dest, html["html_body"])
+    sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 
 
