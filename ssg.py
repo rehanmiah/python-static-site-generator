b6e366a3a90f08943d0462ee4da31f1c43661e62
rehanmiah <rehan_miah@hotmail.com>
2021-01-23 17:29:18 +0000

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -1,3 +1,4 @@
+import sys
 import typer
 import ssg.parsers
 from ssg.site import Site
@@ -5,3 +6,7 @@ def main(source="content",dest="dist"):
     config={"source":source,"dest":dest, "parsers":[ssg.parsers.ResourceParser(),ssg.parsers.MarkdownParser(),ssg.parsers.ReStructuredTextParser()]}
     Site(**config).build()
 typer.run(main)
+
+@staticmethod
+def error(self, message):
+    sys.stderr.write("\x1b[1;31m{}\n")
